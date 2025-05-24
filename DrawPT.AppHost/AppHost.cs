7ae5a2924b63e3c90ccfd6dae8d9b02989c9ead7
david chong <d4chong@gmail.com>
2025-05-23 21:12:49 -0400

    starter Aspire project

---
+++ DrawPT.AppHost/AppHost.cs
@@ -0,0 +1,13 @@
+﻿var builder = DistributedApplication.CreateBuilder(args);
+
+var weatherApi = builder.AddProject<Projects.DrawPT_MinimalApi>("weatherapi")
+    .WithExternalHttpEndpoints();
+
+builder.AddNpmApp("vue", "../DrawPT.Vue")
+    .WithReference(weatherApi)
+    .WaitFor(weatherApi)
+    .WithHttpEndpoint(env: "PORT")
+    .WithExternalHttpEndpoints()
+    .PublishAsDockerFile();
+
+builder.Build().Run();
