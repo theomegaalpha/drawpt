5d474e262a1f0ce7d79d104432d7bac00f35bf69
david chong <d4chong@gmail.com>
2025-05-26 22:08:10 -0400

    theme shimmer

--- DrawPT.Vue/src/components/room/game/midround/SelectTheme.vue
+++ DrawPT.Vue/src/components/room/game/midround/SelectTheme.vue
@@ -20,9 +20,37 @@ const handleClick = (theme: string) => {
       v-for="(theme, index) in props.themes"
       :key="index"
       @click="handleClick(theme)"
-      class="m-2 cursor-pointer rounded-md border border-gray-400 p-2 hover:bg-gray-200"
+      class="shimmer-glow m-2 cursor-pointer rounded-md border border-gray-400 p-2"
     >
       {{ theme }}
     </div>
   </div>
 </template>
+
+<style scoped>
+.shimmer-glow {
+  text-decoration: none;
+  border: 1px solid rgb(146, 148, 248);
+  position: relative;
+  overflow: hidden;
+}
+
+.shimmer-glow:hover {
+  box-shadow: 1px 1px 5px 2px rgba(146, 148, 248, 0.4);
+}
+
+.shimmer-glow:before {
+  content: '';
+  position: absolute;
+  top: 0;
+  left: -100%;
+  width: 100%;
+  height: 100%;
+  background: linear-gradient(120deg, transparent, rgba(146, 148, 248, 0.4), transparent);
+  transition: all 650ms;
+}
+
+.shimmer-glow:hover:before {
+  left: 100%;
+}
+</style>
