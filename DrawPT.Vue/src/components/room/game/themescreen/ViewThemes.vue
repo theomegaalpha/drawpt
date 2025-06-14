8baa373d35f276ad964889aacc50dd79b7a0b61d
david <d4chong@gmail.com>
2025-06-13 21:23:26 -0400

    more prettify
    * more cleanup
    
    * working lobby
    
    * tweak
    
    * view

--- DrawPT.Vue/src/components/room/game/themescreen/ViewThemes.vue
+++ DrawPT.Vue/src/components/room/game/themescreen/ViewThemes.vue
@@ -19,17 +19,20 @@ onMounted(() => {
 </script>
 
 <template>
-  <div class="text-center text-xl font-bold">
-    <div v-for="(theme, index) in props.themes" :key="index">
-      <div
-        class="bg-surface-default mb-4 animate-blur-in cursor-default rounded-lg px-9 py-3"
-        :style="{
-          animationDelay: `${index * 200}ms`,
-          animationPlayState: animationsReadyToPlay ? 'running' : 'paused',
-          animationFillMode: 'backwards'
-        }"
-      >
-        {{ theme }}
+  <div class="flex min-h-screen flex-col items-center justify-center text-center text-xl font-bold">
+    <h1 class="text-2xl font-bold">Selecting Theme...</h1>
+    <div class="relative mb-16 w-[40rem]">
+      <div v-for="(theme, index) in props.themes" :key="index">
+        <div
+          class="bg-surface-default mb-4 animate-blur-in cursor-default rounded-lg px-9 py-3"
+          :style="{
+            animationDelay: `${index * 200}ms`,
+            animationPlayState: animationsReadyToPlay ? 'running' : 'paused',
+            animationFillMode: 'backwards'
+          }"
+        >
+          {{ theme }}
+        </div>
       </div>
     </div>
   </div>
