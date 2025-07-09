ef89fa2eab0f291ba0c01c3cbc179035659bd0ca
david chong <d4chong@gmail.com>
2025-07-08 20:58:16 -0400

    minor tweak

--- DrawPT.Vue/src/components/common/UnshinyButton.vue
+++ DrawPT.Vue/src/components/common/UnshinyButton.vue
@@ -1,6 +1,6 @@
 <template>
   <div
-    class="radial-gradient relative rounded-full border border-black/25 px-6 py-2 dark:border-white/25"
+    class="radial-gradient relative rounded-full border border-black/20 px-6 py-2 dark:border-white/20"
   >
     <span class="linear-mask relative block h-full w-full font-light tracking-wide">
       <slot>Start now</slot>
